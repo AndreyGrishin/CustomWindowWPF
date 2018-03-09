f8660eef18b1d67f93597250d0325e362b56422a
Андрей Гришин <fearerlcpd@mail.ru>
2018-03-09 21:41:32 +0300

    Add ToggleSwitch control; Add Theme switching

---
+++ CustomWindow/Controls/ToggleSwitch.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomWindow.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ToggleSwitch.xaml
+    /// </summary>
+    public partial class ToggleSwitch : UserControl
+    {
+    
+        #region Dependency Properties
+
+        #region Header
+
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+            "Header",
+            typeof(string),
+            typeof(ToggleSwitch),
+            new FrameworkPropertyMetadata(
+                "Header",
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                OnHeaderChanged));
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        private static void OnHeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region OffContent
+
+        public static readonly DependencyProperty OffContentProperty = DependencyProperty.Register(
+            "OffContent",
+            typeof(string),
+            typeof(ToggleSwitch),
+            new FrameworkPropertyMetadata(
+                "Off",
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                OffContentPropertyChanged));
+
+        public string OffContent
+        {
+            get => (string)GetValue(OffContentProperty);
+            set => SetValue(OffContentProperty, value);
+        }
+
+        private static void OffContentPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+
+        }
+
+        #endregion
+
+        #region OnContent
+
+        public static readonly DependencyProperty OnContentProperty = DependencyProperty.Register(
+            "OnContent",
+            typeof(string),
+            typeof(ToggleSwitch),
+            new FrameworkPropertyMetadata(
+                "On",
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                OnContentPropertyChanged));
+
+
+        public string OnContent
+        {
+            get => (string)GetValue(OnContentProperty);
+            set => SetValue(OnContentProperty, value);
+        }
+
+        private static void OnContentPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+
+        }
+
+        #endregion
+
+        #region IsOn
+
+        public static readonly DependencyProperty IsOnProperty = DependencyProperty.Register(
+            "IsOn",
+            typeof(bool),
+            typeof(ToggleSwitch),
+            new FrameworkPropertyMetadata(
+                false,
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                IsOnPropertyChanged));
+
+        public bool IsOn
+        {
+            get => (bool)GetValue(IsOnProperty);
+            set => SetValue(IsOnProperty, value);
+        }
+
+        private static void IsOnPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Routed Events
+
+        public static readonly RoutedEvent ToggledEvent = EventManager.RegisterRoutedEvent("Toggled",
+            RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ToggleSwitch));
+
+        public event RoutedEventHandler Toggled
+        {
+            add => AddHandler(ToggledEvent, value);
+            remove => RemoveHandler(ToggledEvent, value);
+        }
+
+        #endregion
+
+
+        public ToggleSwitch()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+
+        #region Methods
+
+
+
+        #endregion
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsOn = ToggleButton.IsChecked == true;
+
+            RaiseEvent(new RoutedEventArgs(ToggledEvent));
+        }
+    }
+}
--- CustomWindow/MainWindow.xaml.cs
+++ CustomWindow/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace CustomWindow
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<ResourceDictionary> _resourceDictionaries = new List<ResourceDictionary>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            InitializeThemes();
+        }
+
+
+        private void InitializeThemes()
+        {
+            foreach (var resourceDictionary in Application.Current.Resources.MergedDictionaries.ToArray())
+            {
+                if (resourceDictionary.Source.OriginalString == "Themes/DarkStyle.xaml" &&
+                    ThemeToggleSwitch.IsChecked == false)
+                {
+                    Application.Current.Resources.MergedDictionaries.Remove(resourceDictionary);
+                }
+                _resourceDictionaries.Add(resourceDictionary);
+            }
+        }
+
+
+
+        private void ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
+        {
+            ResourceDictionary darkResourceDictionary = _resourceDictionaries.FirstOrDefault(x => x.Source.OriginalString == "Themes/DarkStyle.xaml");
+            ResourceDictionary lightResourceDictionary = _resourceDictionaries.FirstOrDefault(x => x.Source.OriginalString == "Themes/LightStyle.xaml");
+
+            ResourceDictionary removedStyleDictionary;
+            ResourceDictionary addedStyleDictionary;
+
+            if (ThemesToggleSwitch.IsOn)
+            {
+                addedStyleDictionary = darkResourceDictionary;
+                removedStyleDictionary = lightResourceDictionary;
+            }
+            else
+            {
+                addedStyleDictionary = lightResourceDictionary;
+                removedStyleDictionary = darkResourceDictionary;
+            }
+
+            Application.Current.Resources.MergedDictionaries.Add(addedStyleDictionary);
+
+            Application.Current.Resources.MergedDictionaries.Remove(removedStyleDictionary);
         }
     }
 }
